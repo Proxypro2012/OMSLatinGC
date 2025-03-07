9d961525980c6065a67cf040a738f8de73184fd9
Proxypro2012 <kabir.tiwari@outlook.com>
2025-03-06 22:22:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import streamlit_antd_components as sac
 import os
 import random
 import json
-
+import base64
 
 r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
 r2col1, r2col2, r2col3 = st.columns([1, 2, 0.5])
